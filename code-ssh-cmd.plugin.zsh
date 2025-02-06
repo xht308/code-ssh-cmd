1f25eb23ae67c1c553fdf4a30a89dd8c14ff79cf
xht308 <77667228+xht308@users.noreply.github.com>
2025-02-06 16:20:25 -0600

    Add OSC52 copy functionality to codessh command

--- code-ssh-cmd.plugin.zsh
+++ code-ssh-cmd.plugin.zsh
@@ -10,6 +10,12 @@ if type codessh &>/dev/null; then
     return 0  # If the command already exists, exit the script
 fi
 
+# Function to copy text using OSC52
+osc52_copy() {
+    local data=$(echo -n "$1" | base64 | tr -d '\n')
+    printf "\033]52;c;%s\a" "$data"
+}
+
 # Define the codessh command
 codessh() {
     # If no directory is provided, default to the current directory
@@ -22,9 +28,10 @@ codessh() {
     fi
 
     # Check if the CODE_SSH_CONFIG_NAME environment variable is set
+    local ssh_command
     if [[ -n "$CODE_SSH_CONFIG_NAME" ]]; then
         # If set, use SSH config alias to generate the command
-        echo "code --folder-uri vscode-remote://ssh-remote+$CODE_SSH_CONFIG_NAME$target_dir"
+        ssh_command="code --folder-uri vscode-remote://ssh-remote+$CODE_SSH_CONFIG_NAME$target_dir"
     else
         # Otherwise, use username and remote IP address to generate the command
         local username=${USER}
@@ -37,10 +44,16 @@ codessh() {
         
         # If the port is not the default port 22, add it to the command
         if [[ "$remote_port" != "22" ]]; then
-            echo "code --folder-uri vscode-remote://ssh-remote+$username@$remote_ip:$remote_port$target_dir"
+            ssh_command="code --folder-uri vscode-remote://ssh-remote+$username@$remote_ip:$remote_port$target_dir"
         else
-            echo "code --folder-uri vscode-remote://ssh-remote+$username@$remote_ip$target_dir"
+            ssh_command="code --folder-uri vscode-remote://ssh-remote+$username@$remote_ip$target_dir"
         fi
     fi
+
+    # Print the command to the terminal
+    echo "$ssh_command"
+
+    # Copy the command to the clipboard
+    osc52_copy "$ssh_command"
 }
 
